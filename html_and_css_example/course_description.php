ee53870fdefca195694d96f5b080c9e449d6e718
Rosemina Vohra <rosemina.vohra@njit.edu>
2024-02-02 09:50:02 -0500

    Unit03-Feb02

---
+++ html_and_css_example/course_description.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>Advanced Internet Applications</title>
+    <link rel="stylesheet" href="course_description.css" />
+  </head>
+  <body>
+    <?php include('header.php'); ?>
+    <main>
+      <ul>
+        <li id="first">Subject: IT</li>
+        <li>Number: 302</li>
+        <li>Credit: 3</li>
+        <li>Prerequisite: IT 2020 or IS 218</li>
+        <li>Textbooks:
+          <a href="https://greglim.gumroad.com/l/nhcqq" target="_blank" >Beginner MERN Stack by Greg Lim</a>
+        </li>
+      </ul>
+      <!-- course description from git page -->
+      <p class="details">This course is designed to provide students with an in-depth exploration of advanced web development concepts and tools. The course covers the essential aspects of developing web applications, including utilization of frontend frameworks such as React and/or Angular, interfacing with the backend using RESTful API and/or Graph QL, and integrating NoSQL databases into the architecture. Students will build on previous knowledge and experience in web development and will gain practical application through hands-on projects and assignments. By the end of the course, students will have the skills necessary to design and develop complex web applications that meet industry standards.</p>
+    </main>
+    <?php include('footer.php') ?>
+  <body>
+</html>
---
+++ html_and_css_example/footer.php
@@ -0,0 +1,11 @@
+<style>
+  h5 {
+    color: #272932;
+    background-color: #828489;
+  }
+</style>
+<footer>
+  <h5>
+    <p>Name, Feb 02, IT-202-004, Email: yourid@njit.edu</p>
+  </h5>
+</footer>
