75a77bd452601161de7d665804df2f65d915e454
Rosemina Vohra <rosemina.vohra@njit.edu>
2024-02-21 09:29:37 -0500

    Feb21

--- phpchapter04/product_list.php
+++ phpchapter04/product_list.php
@@ -1,52 +1,95 @@
 <?php
-  // Slide 26
-  // USE database_local OR database_njit, which ever one works!
-  require_once('database_local.php');
+// Use database_local.php OR database_njit.php
+require_once('database_local.php');
 
-  // Get category ID
-  $category_id = filter_input(INPUT_GET, 'category_id', FILTER_VALIDATE_INT);
-  if($category_id == NULL || $category_id == FALSE) {
-    $category_id = 1;
-  }
+// Get category ID
+$category_id = filter_input(INPUT_GET, 'category_id', FILTER_VALIDATE_INT);
+if ($category_id == NULL || $category_id == FALSE) {
+  $category_id = 1;
+}
 
-  $queryCategory = 'SELECT * FROM categories WHERE categoryID = :category_id';
-  $statement1 = $db->prepare($queryCategory);
-  $statement1->bindValue(':category_id', $category_id);
-  $statement1->execute();
-  $category = $statement1->fetch();
-  $category_name = $category['categoryName'];
-  $statement1->closeCursor();
-  // DEBUGGING ONLY
-  // echo "<pre>";
-  // print_r($category);
-  // echo "</pre>";
-  // echo "The category name is $category_name";
+// Get name for selected category
+$queryCategory = 'SELECT * FROM categories
+          WHERE categoryID = :category_id';
+$statement1 = $db->prepare($queryCategory);
+$statement1->bindValue(':category_id', $category_id);
+$statement1->execute();
+$category = $statement1->fetch();
+$category_name = $category['categoryName'];
+$statement1->closeCursor();
 
-  $queryAllCategories = 'SELECT * FROM categories ORDER BY categoryID';
-  $statement2 = $db->prepare($queryAllCategories);
-  $statement2->execute();
-  $categories = $statement2->fetchAll();
-  $statement2->closeCursor();
-  // DEBUGGING ONLY
-  // echo "<pre>";
-  // print_r($categories);
-  // echo "</pre>";
+// Get all categories
+$queryAllCategories = 'SELECT * FROM categories
+             ORDER BY categoryID';
+$statement2 = $db->prepare($queryAllCategories);
+$statement2->execute();
+$categories = $statement2->fetchAll();
+$statement2->closeCursor();
+
+// Get products for selected category
+$queryProducts = 'SELECT * FROM products
+          WHERE categoryID = :category_id
+          ORDER BY productID';
+$statement3 = $db->prepare($queryProducts);
+$statement3->bindValue(':category_id', $category_id);
+$statement3->execute();
+$products = $statement3->fetchAll();
+// DEBUGGING ONLY
+// echo "<pre>";
+// print_r($products);
+// echo "</pre>";
+// DEBUGGING ONLY
+$statement3->closeCursor();
 ?>
+<!DOCTYPE html>
 <html>
-  <head>
-    <title>My Guitar Shop</title>
-  </head>
-  <body>
-    <main>
-      <h1>Product List</h1>
-      <h2>Categories</h2>
-      <ul>
-        <?php foreach( $categories as $category) : ?>
-          <li>
-            <a href="?category_id=<?php echo $category['categoryID']; ?>"><?php echo $category['categoryName']; ?></a>
-          </li>
-        <?php endforeach ?>
-      </ul>
-    </main>
-  </body>
+<!-- the head section -->
+<head>
+  <title>My Guitar Shop</title>
+  <link rel="stylesheet" href=product_list.css />
+</head>
+
+<!-- the body section -->
+<body>
+<main>
+  <h1>Product List</h1>
+  <aside>
+    <!-- display a list of categories -->
+    <h2>Categories</h2>
+    <nav>
+    <ul>
+      <?php foreach ($categories as $category) : ?>
+      <li>
+        <a href="?category_id=<?php 
+            echo $category['categoryID']; 
+            ?>">
+          <?php echo $category['categoryName']; ?></a>
+      </li>
+      <?php endforeach; ?>
+    </ul>
+    </nav>       
+  </aside>
+
+  <section>
+    <!-- display a table of products -->
+    <h2><?php echo $category_name; ?></h2>
+    <table>
+      <tr>
+        <th>Code</th>
+        <th>Name</th>
+        <th>Price</th>
+      </tr>
+
+      <?php foreach ($products as $product) : ?>
+      <tr>
+        <td><?php echo $product['productCode']; ?></td>
+        <td><?php echo $product['productName']; ?></td>
+        <td><?php echo $product['listPrice']; ?></td>
+      </tr>
+      <?php endforeach; ?>      
+    </table>
+  </section>
+</main>  
+<footer></footer>
+</body>
 </html>
