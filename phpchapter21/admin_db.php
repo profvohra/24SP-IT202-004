8947b2f13a2c982785696c9d0c5239042c445dd2
Rosemina Vohra <rosemina.vohra@njit.edu>
2024-02-28 09:32:25 -0500

    Feb28

---
+++ phpchapter21/admin_db.php
@@ -0,0 +1,20 @@
+<?php
+// MODIFY to use database_local.php or database_njit.php
+require_once('database_local.php');
+
+function is_valid_admin_login($email, $password) {
+  $db = getDB();
+  $query = 'SELECT password FROM administrators WHERE emailAddress = :email';
+  $statement = $db->prepare($query);
+  $statement->bindValue(':email', $email);
+  $statement->execute();
+  $row = $statement->fetch();
+  $statement->closeCursor();  
+  if ($row === false) {
+    return false;
+  } else {
+    $hash = $row['password'];
+    return password_verify($password, $hash);
+  }
+}
+?>
---
+++ phpchapter21/authenticate.php
@@ -0,0 +1,19 @@
+<?php
+  require_once('admin_db.php');
+  session_start();
+
+  $email = filter_input(INPUT_POST, 'email');
+  $password = filter_input(INPUT_POST, 'password');
+  if (is_valid_admin_login($email, $password)) {
+    $_SESSION['is_valid_admin'] = true;
+    // redirect logged in user to default page
+    echo "<p>You have successfully logged in.</p>";
+  } else {
+  if ($email == NULL && $password == NULL) {
+    $login_message ='You must login to view this page.';
+  } else {
+    $login_message = 'Invalid credentials.';
+  }
+    include('login.php');
+  }
+?>
